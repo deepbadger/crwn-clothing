abc8c25fec9ecce48ab46bcfe85891cdf53d495f
Dmitry Ermolenko <deep1@list.ru>
2020-07-13 14:07:04 +0500

    added redux and implemented userReducer and UserActions to header and App components

--- src/App.js
+++ src/App.js
@@ -1,40 +1,38 @@
 import React, {Component} from 'react';
 import {Switch, Route} from 'react-router-dom'
 
+import {connect} from 'react-redux';
+
 import './App.css';
 import HomePage from './pages/homepage/homepage.component'
 import ShopPage from './pages/shop/shop.component'
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
 import Header from "./components/header/header.component";
 import {auth, createUserProfileDocument} from './firebase/firebase.utils'
-
+import {setCurrentUser} from './redux/user/user.actions'
 
 class App extends Component {
-    constructor() {
-        super();
-        this.state = {
-            currentUser: null
-        }
-    }
 
     unsubscribeFromAuth = null
 
     componentDidMount() {
-        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
-            if (userAuth) {
-                const userRef = await createUserProfileDocument(userAuth);
+        const {setCurrentUser} = this.props;
 
-                userRef.onSnapshot(snapShot => {
-                    this.setState({
-                        currentUser: {
+        this.unsubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+                if (userAuth) {
+                    const userRef = await createUserProfileDocument(userAuth);
+
+                    userRef.onSnapshot(snapShot => {
+                        setCurrentUser({
                             id: snapShot.id,
                             ...snapShot.data()
-                        }
+                        });
                     });
-                });
+                }
+
+                setCurrentUser(userAuth);
             }
-            this.setState({currentUser: userAuth});
-        });
+        );
     }
 
     componentWillUnmount() {
@@ -44,7 +42,7 @@ class App extends Component {
     render() {
         return (
             <div>
-                <Header currentUser={this.state.currentUser}/>
+                <Header/>
                 <Switch>
                     <Route exact path='/' component={HomePage}/>
                     <Route path='/shop' component={ShopPage}/>
@@ -55,4 +53,8 @@ class App extends Component {
     }
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+    setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
